2e055313010bd28e2bf04b608c2cd68ebd6cd1c0
Leo Diaz <leoangelo.diaz@gmail.com>
2016-10-18 17:02:51 +0000

    add test code

--- app/landing/controllers/landing.controller.js
+++ app/landing/controllers/landing.controller.js
@@ -6,16 +6,16 @@ export default class LandingController {
 	/* @ngInject */
 	constructor($scope){
     $scope.scopeObj = {
-      value : "Using webpack dev server. Test 123"
+      value : "Using webpack dev server. Hmm"
     };
-
+    console.log("reload");
     this.thisObj = {
       value: "Using this"
     };
 	}
 
   helloWord() {
-    console.log("Hello World 123");
+    console.log("Hello World 1234");
   }
 
 }
