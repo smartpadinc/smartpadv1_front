22f68f7d857dc46f5f6f6e8d565a1dea813a1f0a
Leo Diaz <leoangelo.diaz@gmail.com>
2016-11-05 08:47:04 +0000

    restructure

--- app/landing/landing.module.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-import {default as LandingController} from './controllers/landing.controller';
-import angular from 'npm/angular';
-
-export default angular
-	.module('app.landing', [])
-	.controller('LandingController', LandingController);
