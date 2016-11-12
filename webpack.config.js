4110f78b963415e32a5a7e067dd2151c10b1b0c2
Leo Diaz <leoangelo.diaz@gmail.com>
2016-11-12 15:43:09 +0000

    add webpack production (ongoing)

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,14 @@
 'use strict';
 
-module.exports = require('./webpack-config/webpack.dev');
+switch(process.env.SMRT_ENV) {
+  case 'development':
+    console.log("Starting webpack development server...");
+    module.exports = require('./webpack-config/webpack.dev');
+    break;
+  case 'production':
+    console.log("Building changes...");
+    module.exports = require('./webpack-config/webpack.prod');
+    break;
+  default:
+    console.log("environment not found");
+}
